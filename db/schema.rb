ecbd517165a1a77fc5505896e8bfa4efe1cf3ca8
Jmateus88 <joaomlmateus@gmail.com>
2016-10-06 23:25:59 +0100

    4.4 debugging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002201457) do
+ActiveRecord::Schema.define(version: 20161006220000) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
