dd4e7fb067258ca00f705e526616021dfe3b135a
Jmateus88 <joaomlmateus@gmail.com>
2016-10-18 02:32:18 +0100

    5.1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
+  #root 'static_pages#landing_page'
+
+  root 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
