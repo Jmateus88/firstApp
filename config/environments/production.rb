e14b5d535237419fb4759362e976222333676cd1
Jmateus88 <joaomlmateus@gmail.com>
2016-12-17 17:00:10 +0000

    6.13 action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,6 @@ Rails.application.configure do
                      :socket_failure_delay => 0.2,
                      :down_retry_delay => 60
                     }
+  config.web_socket_server_url = "wss://hvnc.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://hvnc.herokuapp.com', 'http://hvnc.herokuapp.com']                  
 end
